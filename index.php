7f793ff06afc93a6da73428a20eb494bfaf4f206
Shailesh744 <73304133+Shailesh744@users.noreply.github.com>
2023-12-13 16:15:25 +0530

    name/

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>include</title>
+</head>
+<body>
+    <h1>this is example of include</h1>
+    <p>below in website ,inlcude will be added</p>
+    <? php include 'index1.php';
+    ?>
+</body>
+</html>
